7035be36eb8fe2f8d3999de2d2772ef2d90c184a
mmibragimov <115887152+mmibragimov@users.noreply.github.com>
2022-11-26 20:48:40 +0300

    Add files via upload

---
+++ labs/lab07/zadaniye.asm
@@ -0,0 +1,30 @@
+%include 'in_out.asm'
+SECTION .data
+msg DB 'X = ',0
+ans DB 'ANSWER',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+
+_start:
+mov eax,msg
+call sprintLF
+mov ecx,x
+mov edx,80
+call sread
+mov eax,x
+call atoi
+
+mov ebx,31
+mul ebx
+sub eax,5
+add eax,10
+
+mov ebx,eax
+mov eax,ans
+call sprint
+mov eax,ebx
+call iprintLF
+
+call quit
